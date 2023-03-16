b698b63c804b71e7e375cf7533072fe1be56be35
Mastafa <mastafa2618@gmail.com>
2023-03-16 11:17:22 +0000

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+
+    static int a = 10;
+    static int b = 5;
+    static int answer;
+
+    public static void main(String[] args) {
+        addition();
+    }
+    public static void addition(){
+        answer = a+b;
+        System.out.println(answer);
+    }
+
+}
---
+++ src/Multiplication.java
@@ -0,0 +1,15 @@
+public class Multiplication {
+
+    static int a = 10;
+    static int b = 5;
+    static int answer;
+
+    public static void main(String[] args) {
+        multiplication();
+    }
+
+    public static void multiplication(){
+        answer = a*b;
+        System.out.println(answer);
+    }
+}
